b724cb0d6956c67021ef3a41f2333c7cb828481a
mr chapai <myicskt@gmail.com>
2024-02-13 12:32:42 -0600

    after adding do while loop

--- src/practical_02/MainDictionary.java
+++ src/practical_02/MainDictionary.java
@@ -22,29 +22,34 @@ public class MainDictionary {
 		reader = new WordAndMeaningReader(file);
 		map = new HashMap();
 		scanner = new Scanner(System.in);
-		System.out.println("Enter the word to get meaning ");
-		System.out.println("example aple,table,orange ");
 
-		String input = scanner.next().toLowerCase();
+		do {
 
-		if (filechecker.checkFileIsExist() == true) {
+			System.out.println("Enter the word to get meaning ");
+			System.out.println("Example aple,table,orange ");
 
-			map = reader.dictionaryData();
-			if (map.containsKey(input) == true) {
-				System.out.println(input.toUpperCase());
-				String[] meaning = map.get(input).split(",");
-				for (int i = 0; i < meaning.length; i++) {
-					System.out.println("\t meaning " + (i + 1) + " : " + meaning[i].trim());
+			String input = scanner.next().toLowerCase();
+
+			if (filechecker.checkFileIsExist() == true) {
+
+				map = reader.dictionaryData();
+				if (map.containsKey(input) == true) {
+					System.out.println(input.toUpperCase());
+					String[] meaning = map.get(input).split(",");
+					for (int i = 0; i < meaning.length; i++) {
+						System.out.println("\t meaning " + (i + 1) + " : " + meaning[i].trim());
+					}
+				} else {
+					System.out.println("could not found the word : " + input);
 				}
 			} else {
-				System.out.println("could not found the word : " + input);
+				System.out.println("File is not exist ");
+				System.out.println("Curent file path: " + filepath);
+
 			}
-		} else {
-			System.out.println("File is not exist ");
-			System.out.println("Curent file path: " + filepath);
+			System.out.println("********************************************");
+			System.out.println("Enter \"y\" to find next");
 
-		}
-
+		} while (scanner.next().equalsIgnoreCase("y"));
 	}
-
 }
